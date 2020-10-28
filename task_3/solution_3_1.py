15eff69f582c84b4b00a08fa25f0a75b45e6fa89
polina-malakhova <72040802+polina-malakhova@users.noreply.github.com>
2020-10-28 19:56:04 +0300

    Update solution_3_1.py

--- task_3/solution_3_1.py
+++ task_3/solution_3_1.py
@@ -1,4 +1,10 @@
-# your code here
-user_input = input()
-# your code here
-print(alphabet)
+a=input()
+alphabet=""
+alphabet1=str(alphabet)
+for i in a:
+	if i not in alphabet:
+		alphabet=alphabet + str(i)
+		alphabet1=str(alphabet)
+	else:
+		alphabet1=str(alphabet)
+print(alphabet1)
