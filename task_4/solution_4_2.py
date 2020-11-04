90293ed4b24228a4f0f9717ae8b6ad0e87bb680e
polina-malakhova <72040802+polina-malakhova@users.noreply.github.com>
2020-11-04 17:48:10 +0300

    Update solution_4_2.py

--- task_4/solution_4_2.py
+++ task_4/solution_4_2.py
@@ -1,6 +1,37 @@
-# your code here
-for smth in 'a'*10:
-    user_input = input()
-    # your code here
-    if my_cool_condition == True:
-        print(form, lemma)
+for i in "a"*10:
+    user_in=input()
+    bad_string=""
+    if user_in!=bad_string and user_in[0]!="#":
+
+        num_of_tab = 0
+        form = ""
+
+        for letter in user_in:
+            if num_of_tab == 1:
+                form = form + letter
+
+            if letter=="\t":
+                num_of_tab += 1
+
+        num_of_tab=0
+        lemma = ""
+
+        for letter in user_in:
+            if num_of_tab == 2:
+                lemma=lemma + letter
+
+            if letter=="\t":
+                num_of_tab += 1
+
+        number_of_letter=0
+        wow_condition=""
+
+        for i in lemma:
+                if lemma[number_of_letter]==form[number_of_letter]:
+                    number_of_letter+=1
+
+                if number_of_letter==len(lemma)-1:
+                    wow_condition="True"
+        
+        if wow_condition!="True":
+            print(form,lemma)
